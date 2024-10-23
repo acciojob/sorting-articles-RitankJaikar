813c0d95230ad624569b319457fa05e8e46ef456
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-10-23 16:30:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+let bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+const ul= document.querySelector("#band");
+
+function stripArticle(bandName) {
+	return bandName.replace(/^(a |an |the |A |An |The )/i, '').trim();
+}
+
+bands= bands.sort((a,b) => {
+	//console.log("a",stripArticle(a));
+	//console.log("b",stripArticle(b));
+	return stripArticle(a).localeCompare(stripArticle(b));
+});
+//console.log(bands);
+
+bands.forEach(band => {
+	const li= document.createElement("li");
+	li.innerText= band;
+	ul.append(li);
+})
+
+
+
+
